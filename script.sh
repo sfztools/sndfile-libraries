84e1fbc2e7fb9d9eb4a1fadb37a67f828ac5747f
redtide <redtid3@gmail.com>
2020-02-22 07:25:58 +0100

    Updated script

--- script.sh
+++ script.sh
@@ -1,34 +1,36 @@
 sudo apt-get install libjack-jackd2-dev lv2-dev pkg-config automake gettext libtool-bin cmake
 apt-get source libogg-dev libvorbis-dev libflac-dev libsndfile
 
-prefix=$(pwd)/prefix
-flags="-fPIC -DPIC -fvisibility=hidden -I${prefix}/include"
+DESTDIR=${PWD}/${INSTALL_DIR}
+PREFIX=/usr/local
+FLAGS="-fPIC -DPIC -fvisibility=hidden -I${PREFIX}/include"
 
-mkdir -p $prefix
+mkdir -p ${DESTDIR}
+
 pushd libogg-1.3.2
-CFLAGS=$flags CXXFLAGS=$flags PKG_CONFIG_LIBDIR=$prefix/lib/pkgconfig ./configure --disable-shared --prefix=$prefix
+CFLAGS=${FLAGS} CXXFLAGS=${FLAGS} PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig ./configure --disable-shared --prefix=${PREFIX}
 make -j$(nproc)
-make install
+make install DESTDIR=${DESTDIR}
 popd
 
 pushd libvorbis-1.3.5
 ./autogen.sh
-CFLAGS=$flags CXXFLAGS=$flags PKG_CONFIG_LIBDIR=$prefix/lib/pkgconfig ./configure --disable-shared --prefix=$prefix
+CFLAGS=${FLAGS} CXXFLAGS=${FLAGS} PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig ./configure --disable-shared --prefix=${PREFIX}
 make -j$(nproc)
-make install
+make install DESTDIR=${DESTDIR}
 popd
 
 pushd flac-1.3.2
 ./autogen.sh
-CFLAGS=$flags CXXFLAGS=$flags PKG_CONFIG_LIBDIR=$prefix/lib/pkgconfig ./configure --disable-shared --prefix=$prefix
+CFLAGS=${FLAGS} CXXFLAGS=${FLAGS} PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig ./configure --disable-shared --prefix=${PREFIX}
 make -j$(nproc)
-make install
+make install DESTDIR=${DESTDIR}
 popd
 
 pushd libsndfile-1.0.28
-CFLAGS=$flags CXXFLAGS=$flags PKG_CONFIG_LIBDIR=$prefix/lib/pkgconfig ./configure --disable-shared --disable-full-suite --prefix=$prefix
+CFLAGS=${FLAGS} CXXFLAGS=${FLAGS} PKG_CONFIG_LIBDIR=${PREFIX}/lib/pkgconfig ./configure --disable-shared --disable-full-suite --prefix=${PREFIX}
 make -j$(nproc)
-make install
+make install DESTDIR=${DESTDIR}
 popd
 
-tar -zcvf "${INSTALL_DIR}.tar.gz" $prefix
+tar -zcvf "${INSTALL_DIR}.tar.gz" ${INSTALL_DIR}
